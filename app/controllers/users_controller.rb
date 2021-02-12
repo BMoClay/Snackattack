349dad4988f99b576535b5afbe6774aac6118954
therunningagent <luistjr@gmail.com>
2021-02-12 10:06:56 -0700

    added update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,12 @@ class UsersController < ApplicationController
         render json: user
     end 
 
+    def me
+        # checking some identifying info about the request 
+        user = User.first
+        render json:user
+    end 
+
     def show 
         user = User.find(params[:id])
         render json: user
@@ -21,7 +27,7 @@ class UsersController < ApplicationController
     end 
 
     def update
-        user = User.find(params[:id])
+        user = User.first
         user.update(user_params)
         render json: user
     end 
@@ -34,7 +40,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params 
-        params.permit(:name, :age, :lifestyle, :stash )
+        params.permit(:name, :age, :lifestyle)
     end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :snacks
   resources :users
 
+  get "/me", to: "users#me"
   post "/login", to: "users#login"
-  
+  patch "/me", to: "users#update"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
