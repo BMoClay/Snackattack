8cf6722498173a1003ce92198401ef1eeb554b8e
BMoClay <benmorgancleveland@gmail.com>
2021-02-16 12:44:09 -0500

    favorites controller create function add

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,8 +10,20 @@ class FavoritesController < ApplicationController
       render json: favorite 
     end
 
-    def stash
-      user = User.find_by(params[:user_id])
-      render json: user.stash
-    end
+    # def stash
+    #   user = User.find_by(params[:user_id])
+    #   render json: user.stash
+    # end
+
+    def create
+      favorite = Favorite.create(favorite_params)
+      render json: favorite 
+    end 
+
+    private 
+
+    def favorite_params 
+        params.permit(:user_id, :snack_id,)
+    end 
+
 end
