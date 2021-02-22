b853209a75c80025d6e8b0d13b375123ca5db033
therunningagent <luistjr@gmail.com>
2021-02-21 20:04:35 -0700

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://snackattack.netlify.app/https://example.com'
+    origins 'snackattack.netlify.app'
 
     resource '*',
       headers: :any,
